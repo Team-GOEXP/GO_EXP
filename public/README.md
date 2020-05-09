141031dc34515f97733d06a5581529c82267b40d
teamGOEXP <contactogoexp@gmail.com>
2020-05-09 16:04:41 +0200

    first commit

---
+++ public/README.md
@@ -0,0 +1,2 @@
+# GOEXP
+en este repositorio estaremos montando todo el codigo de la parte Frontend del proyecto, con el fin de que los colaboradores puedan hacer cambios necesarios
---
+++ public/scripts/main.js
@@ -0,0 +1,23 @@
+//de aca empieza el codigo para el control del muno hamburguesa en un dispositivo movil
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click",function(){
+    if(contador == 0){
+        enlaces.className = ('enlaces dos');
+        contador=1;
+    }else{
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+        contador = 0;
+    }
+})
+
+window.addEventListener('resize', function(){
+    if(this.screen.width > 750){
+        contador = 0;
+        enlaces.classList.remove('dos');
+        enlaces.className = ('enlaces uno');
+    }
+})
